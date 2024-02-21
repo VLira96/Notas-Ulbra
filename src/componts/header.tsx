952c68a3955edcf2b3200f00f61efb37fca6418f
VLira96 <lira94634@gmail.com>
2024-02-21 13:55:03 -0300

    correção de responsividade

--- src/componts/header.tsx
+++ src/componts/header.tsx
@@ -2,7 +2,7 @@ import ButtonLinks from "./buttonLinks";
 
 const Header = () => {
     return (
-        <header className="bg-secundaria h-18 flex items-center justify-center">
+        <header className="bg-secundaria h-20 flex items-center justify-center">
             <nav className="flex gap-2 lg:gap-48 m-4">
                 <ButtonLinks caminho='/' text='Entenda a nota' />
                 <ButtonLinks caminho='/nota/semrecuperacao' text='Nota sem recuperação' />
