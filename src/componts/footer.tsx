2f37a0c1b091bd1ee6c3fa99589d34d9a2b4ae07
VLira96 <lira94634@gmail.com>
2024-04-09 14:55:00 -0300

    tirei meu credito do footer

--- src/componts/footer.tsx
+++ src/componts/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="h-16 bg-secundaria flex items-center justify-center">
-            <p className="text-xl">Desenvolvido por Vinícius Lira</p>
+            <p className="text-xl">Desenvolvido por um aluno ai kkkkkkkkkkkkk</p>
         </footer>
     );
 }
